2c6b6f047f48998352b26ed2e51f5236d6034d0a
Surendravarmaa <jampana.varma@ad.infosys.com>
2024-04-17 19:04:55 +0530

    f

--- variable.sh
+++ variable.sh
@@ -2,10 +2,6 @@
 
 read -p "Enter your name : " username 
 
-echo "$username"
-
 read -p "Enter your password : " password
-
-echo "$password"
 
 echo "username is $username and password is $password"
