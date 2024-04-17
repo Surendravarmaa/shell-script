08603695a5b812e1a9d737b8cd87ac7ea28a62f3
Surendravarmaa <jampana.varma@ad.infosys.com>
2024-04-17 14:27:29 +0530

    changed

---
+++ Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=SURESH
+PERSON2=RAMESH
+
+echo "$PERSON1::Hi $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, i am fine how are you?"
+echo "$PERSON1:: i am fine, how is your work going on?"
+echo "$PERSON2:: its good, i am learning devops" 
