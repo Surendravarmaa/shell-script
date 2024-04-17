3bfcce6908ab18d0d66bcddab3a82fcedc5ce2fb
Surendravarmaa <jampana.varma@ad.infosys.com>
2024-04-17 18:10:59 +0530

    changes

---
+++ important.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Current working directory : $PWD"
+echo "All variables : $@"
+echo "Number of variables passed: $#"
+echo "Script name : $0"
+echo "Prsent working directory : $PWD"
+echo "Which user is running this script : $USER"
+echo "Hostname : $HOSTNAME"
+echo "Process ID of the current shell script : $$"
+echo "Process ID of the last background command : $!"
+
