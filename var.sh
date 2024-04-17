8853c97c1c86fd54d2cfec96edde082e876ebc50
Surendravarmaa <jampana.varma@ad.infosys.com>
2024-04-17 15:25:58 +0530

    c

---
+++ var.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::Hi $PERSON2, how are you?"
+echo "$PERSON2:: Hi $PERSON1, i am fine how are you?"
+echo "$PERSON1:: i am fine, how is your work going on?"
+echo "$PERSON2:: its good, i am learning devops"
+echo "$PERSON2:: its good, i am learning devops"
