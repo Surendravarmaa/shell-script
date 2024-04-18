b47c5a74aa7d2e88bbd94285299659285902e719
Surendravarmaa <jampana.varma@ad.infosys.com>
2024-04-18 13:54:10 +0530

    changes

--- installations.sh
+++ installations.sh
@@ -4,8 +4,22 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "please run this command with root access"
+    exit 1
 else 
     echo "you are a super user"
 fi 
 
 dnf install nginx -y
+
+if [ $? -ne 0 ]
+then 
+    echo "my nginx server is failure"
+    exit 1
+else
+    echo "my nginx server is success"
+fi
+
+
+
+
+
