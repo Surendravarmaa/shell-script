d3effcbea6a9c3f68c780611c4a1d22dc7bddd67
Surendravarmaa <jampana.varma@ad.infosys.com>
2024-04-23 16:03:59 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -3,15 +3,15 @@
 USERID=$(id -u)
 
 VALIDATE(){
-    if [ $1 -e 0 ]
+    if [ $1 -ne 0 ]
     then 
-        echo "$2...SUCCESS"
-    else
         echo "$2...FAILURE"
+    else
+        echo "$2...SUCCESS"
     fi
 }
 
-if [ $USERID -e 0 ]
+if [ $USERID -ne 0 ]
 then
     echo "please run this script with root user"
     exit 1
