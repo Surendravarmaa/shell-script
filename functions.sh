d11831c7479049c84f63d1f9186e076af2370fce
Surendravarmaa <jampana.varma@ad.infosys.com>
2024-04-23 17:00:25 +0530

    functions

--- functions.sh
+++ functions.sh
@@ -2,13 +2,14 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date+%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | AWK -F "." '{PRINT -1F}')
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
         echo "$2...FAILURE"
+        exit 1
     else
         echo "$2...SUCCESS"
     fi
@@ -22,8 +23,8 @@ else
     echo "you are a super user"
 fi
 
-dnf install nginx -y &>> $LOGFILE
+dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Installing nginx"
 
-dnf install mysql -y &>> $LOGFILE
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing mysql"
