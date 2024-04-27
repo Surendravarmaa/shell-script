ec649c1fd0001939b7fb104979f444b49618fa3d
Surendravarmaa <jampana.varma@ad.infosys.com>
2024-04-27 15:02:00 +0530

    function

---
+++ 17-delete-oldlogs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SOURCE_DIRECTORY=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+    echo -e "$G source directory exists $N"
+else 
+    echo -e "$R make sure $SOURCE_DIRECTORY exists $N"
+fi
+
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+echo "files to delete: $FILES"
+
